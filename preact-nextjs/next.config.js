b8f3932c8b5069c69292dcaeae82a5e030154a08
Thanakorn <thanakorn.ariyagusolsuthi@prudential.co.th>
2021-04-18 13:52:30 +0700

    perf(preact): integrate preact config in webpack

---
+++ preact-nextjs/next.config.js
@@ -0,0 +1,16 @@
+const nextConfig = {
+  webpack: (config, { isServer, dev }) => {
+    // Replace React with Preact only in client production build
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      })
+    }
+
+    return config
+  },
+}
+
+module.exports = nextConfig
